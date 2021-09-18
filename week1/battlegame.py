1bf69bf3e427386841e0d2945658c57899399f59
Ola Bamisaiye <code@olab.dev>
2021-09-18 11:39:36 -0400

    Update battlegame.py
    Task 2 Completed

--- week1/battlegame.py
+++ week1/battlegame.py
@@ -1,15 +1,25 @@
 # Creating variables for characters: Declared the three variables & I set each of their values to a String with their display name
-var wizard = "Wizard"
-var elf = "Elf"
-var human = "Human"
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
 
-var wizard_hp = 70
-var elf_hp = 100
-var human_hp = 150
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
 
-var wizard_damage = 150
-var elf_damage = 100
-var human_damage = 20
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
 
-var dragon_hp = 300
-var dragon_damage = 50
+dragon_hp = 300
+dragon_damage = 50
+
+# Starting the game
+
+print("Welcome to the Fantasy BattleGame!")
+print()
+print("Here are your character choices: ")
+print("1) Wizard")
+print("2) Elf")
+print("3) Human")
+character = input("Choose your character: ")
