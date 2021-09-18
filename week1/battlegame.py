9f67d201747debde12cdb38351d27a42a6be490a
Ola Bamisaiye <code@olab.dev>
2021-09-18 13:16:01 -0400

    Update battlegame.py
    Bonus Tasks 1, 3, 4, & 5 have been completed

--- week1/battlegame.py
+++ week1/battlegame.py
@@ -1,21 +1,31 @@
+# Name: Ola Bamisaiye
+# Email: Code@olab.dev
+from math import fabs
 import random
+
 # Creating variables for characters: Declared the three variables & I set each of their values to a String with their display name
 wizard = "Wizard"
 elf = "Elf"
 human = "Human"
+orc = "Orc"
 
 wizard_hp = 70
 elf_hp = 100
 human_hp = 150
+orc_hp = 200
 
 wizard_damage = 150
 elf_damage = 100
 human_damage = 20
+orc_damage = 125
 
 dragon_hp = 300
 dragon_damage = 50
 
 # Declaring functions
+def add_empty_line():
+    print()
+
 def get_random_num():
     # Source for random function: https://docs.python.org/3/library/random.html#random.randint
     return random.randint(2, 7)
@@ -43,54 +53,77 @@ def dragon_attack_information(argument):
     return switcher.get(argument, "The Dragon strikes back at the " + str(character))
 
 # Starting the game
+play_game = True
+while play_game:
+    while True:
+        print("-----------------------------------")
+        print("\nWelcome to the Fantasy BattleGame!")
+        add_empty_line()
+        print("Here are your character choices: ")
+        print("1) Wizard")
+        print("2) Elf")
+        print("3) Human")
+        print("4) Orc")
+        print("5) Exit")
+        add_empty_line()
+        character = input("Choose your character: ")
+        if character == "1" or character == "Wizard":
+            character = wizard
+            my_hp = wizard_hp
+            my_damage = wizard_damage
+            break
+        elif character == "2" or character == "Elf":
+            character = elf
+            my_hp = elf_hp
+            my_damage = elf_damage
+            break
+        elif character == "3" or character == "Human":
+            character = human
+            my_hp = human_hp
+            my_damage = human_damage
+            break
+        elif character == "4" or character == "Orc":
+            character = orc
+            my_hp = orc_hp
+            my_damage = orc_damage
+            break
+        elif character == "5":
+            quit()
+        else:
+            print("Unknown Character")
 
-while True:
-    print("-----------------------------------")
-    print("\nWelcome to the Fantasy BattleGame!")
-    print()
-    print("Here are your character choices: ")
-    print("1) Wizard")
-    print("2) Elf")
-    print("3) Human")
-    character = input("Choose your character: ")
-    if character == "1":
-        character = wizard
-        my_hp = wizard_hp
-        my_damage = wizard_damage
-        break
-    elif character == "2":
-        character = elf
-        my_hp = elf_hp
-        my_damage = elf_damage
-        break
-    elif character == "3":
-        character = human
-        my_hp = human_hp
-        my_damage = human_damage
-        break
-    else:
-        print("Unknown Character")
-
-# Source: https://stackoverflow.com/questions/15286401/print-multiple-arguments-in-python
-print("You have chosen the character " + str(character) + "\nHealth: " + str(my_hp) + "\nDamage: " + str(my_damage))
-print("\n" + human_attack_information(1))
-dragon_hp-=my_damage
-print("The dragon's hit points are now: " + str(dragon_hp) + "\n")
-print("" + dragon_attack_information(1))
-my_hp-=dragon_damage
-print("The " + str(character) + "'s hit points are now: " + str(my_hp) + "\n")
-
-#Source: https://pythonguides.com/increment-and-decrement-operators-in-python/#:~:text=loop%20multiple%20conditions-,Python%20decrement%20operator,-Let%20us%20understand
-while True:
-    print("" + human_attack_information(get_random_num()))
+    # Source: https://stackoverflow.com/questions/15286401/print-multiple-arguments-in-python
+    print("You have chosen the character: " + str(character) + "\nHealth: " + str(my_hp) + "\nDamage: " + str(my_damage))
+    print("\n" + human_attack_information(1))
     dragon_hp-=my_damage
     print("The dragon's hit points are now: " + str(dragon_hp) + "\n")
-    if dragon_hp <= 0:
-        print("The Dragon has lost the battle")
-        break
-    print("" + dragon_attack_information(get_random_num()))
+    print("" + dragon_attack_information(1))
     my_hp-=dragon_damage
     print("The " + str(character) + "'s hit points are now: " + str(my_hp) + "\n")
-    if my_hp <= 0:
-        print("The " + str(character) + " has lost the battle")
-        break
+
+    #Source: https://pythonguides.com/increment-and-decrement-operators-in-python/#:~:text=loop%20multiple%20conditions-,Python%20decrement%20operator,-Let%20us%20understand
+    while True:
+        print("" + human_attack_information(get_random_num()))
+        dragon_hp-=my_damage
+        print("The dragon's hit points are now: " + str(dragon_hp) + "\n")
+        if dragon_hp <= 0:
+            print("The Dragon has lost the battle")
+            break
+        print("" + dragon_attack_information(get_random_num()))
+        my_hp-=dragon_damage
+        print("The " + str(character) + "'s hit points are now: " + str(my_hp) + "\n")
+        if my_hp <= 0:
+            print("The " + str(character) + " has lost the battle")
+            break
+    while True:
+        continue_playing = input("Would you like to continue playing the game? (yes,1,true or no,0,false): ")
+        if isinstance(continue_playing, str):
+            if continue_playing == "1" or continue_playing == "true" or continue_playing == "yes":
+                play_game = True
+                break
+            elif continue_playing == "0" or continue_playing == "false" or continue_playing == "no":
+                play_game = False
+                break
+            else:
+                print("A valid option was not provided please try again")
+
