cff79984005782444b91784942090df04fcfc78e
Ola Bamisaiye <code@olab.dev>
2021-09-18 11:27:50 -0400

    Update battlegame.py
    Completed Task 1

---
+++ week1/battlegame.py
@@ -0,0 +1,15 @@
+# Creating variables for characters: Declared the three variables & I set each of their values to a String with their display name
+var wizard = "Wizard"
+var elf = "Elf"
+var human = "Human"
+
+var wizard_hp = 70
+var elf_hp = 100
+var human_hp = 150
+
+var wizard_damage = 150
+var elf_damage = 100
+var human_damage = 20
+
+var dragon_hp = 300
+var dragon_damage = 50
