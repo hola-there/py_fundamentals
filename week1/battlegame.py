9b35317fe7171b3e29ac62ac0947e794592dd20c
Ola Bamisaiye <code@olab.dev>
2021-09-19 13:41:46 -0400

    Update battlegame.py
    Small updates

--- week1/battlegame.py
+++ week1/battlegame.py
@@ -4,12 +4,16 @@ from math import fabs
 import random
 
 # Declaring functions
+
+
 def add_empty_line():
     print()
+
 
 def get_random_num():
     # Source for random function: https://docs.python.org/3/library/random.html#random.randint
     return random.randint(2, 7)
+
 
 def human_attack_information(argument):
     new_switcher = {
@@ -22,6 +26,7 @@ def human_attack_information(argument):
     }
     return new_switcher.get(argument, "The " + str(character) + " attacks the Dragon!")
 
+
 def dragon_attack_information(argument):
     switcher = {
         1: "The dragon eyes the " + str(character) + ". Makes it somersault and then looks on with hunger & anger in it's eye",
@@ -33,8 +38,10 @@ def dragon_attack_information(argument):
     }
     return switcher.get(argument, "The Dragon strikes back at the " + str(character))
 
+
 # Starting the game
 play_game = True
+# Game loop beings
 while play_game:
     # Creating variables for characters: Declared the three variables & I set each of their values to a String with their display name
     wizard = "Wizard"
@@ -54,6 +61,7 @@ while play_game:
 
     dragon_hp = 300
     dragon_damage = 50
+    # The game output starts iwht choices and an introduction
     while True:
         print("-----------------------------------")
         print("\nWelcome to the Fantasy BattleGame!")
@@ -90,39 +98,44 @@ while play_game:
             quit()
         else:
             print("Unknown Character")
-
+    # Character and Dragon attack one another for the first time
     # Source: https://stackoverflow.com/questions/15286401/print-multiple-arguments-in-python
-    print("You have chosen the character: " + str(character) + "\nHealth: " + str(my_hp) + "\nDamage: " + str(my_damage))
+    print("You have chosen the character: " + str(character) +
+          "\nHealth: " + str(my_hp) + "\nDamage: " + str(my_damage))
     print("\n" + human_attack_information(1))
-    dragon_hp-=my_damage
+    dragon_hp -= my_damage
     print("The dragon's hit points are now: " + str(dragon_hp) + "\n")
     print("" + dragon_attack_information(1))
-    my_hp-=dragon_damage
+    my_hp -= dragon_damage
     print("The " + str(character) + "'s hit points are now: " + str(my_hp) + "\n")
 
-    #Source: https://pythonguides.com/increment-and-decrement-operators-in-python/#:~:text=loop%20multiple%20conditions-,Python%20decrement%20operator,-Let%20us%20understand
+    # Source: https://pythonguides.com/increment-and-decrement-operators-in-python/#:~:text=loop%20multiple%20conditions-,Python%20decrement%20operator,-Let%20us%20understand
+    # The character and dragon continue atacking one another until one is defeated
     while True:
         print("" + human_attack_information(get_random_num()))
-        dragon_hp-=my_damage
+        dragon_hp -= my_damage
         print("The dragon's hit points are now: " + str(dragon_hp) + "\n")
         if dragon_hp <= 0:
             print("The Dragon has lost the battle")
             break
         print("" + dragon_attack_information(get_random_num()))
-        my_hp-=dragon_damage
-        print("The " + str(character) + "'s hit points are now: " + str(my_hp) + "\n")
+        my_hp -= dragon_damage
+        print("The " + str(character) +
+              "'s hit points are now: " + str(my_hp) + "\n")
         if my_hp <= 0:
             print("The " + str(character) + " has lost the battle")
             break
+    # it is determined whether a player would like to continue playing the game or not
     while True:
-        continue_playing = input("Would you like to continue playing the game? (yes,1,true or no,0,false): ")
+        continue_playing = input(
+            "Would you like to continue playing the game? (yes,1,true or no,0,false): ")
         if isinstance(continue_playing, str):
             if continue_playing == "1" or continue_playing == "true" or continue_playing == "yes":
                 play_game = True
                 break
             elif continue_playing == "0" or continue_playing == "false" or continue_playing == "no":
                 play_game = False
+                print("Hope you enjoyed the game!\n Goodbye!")
                 break
             else:
                 print("A valid option was not provided please try again")
-
