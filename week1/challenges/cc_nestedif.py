42482d03fc6f41e9bfc6af69191a0ab120dcb75f
Ola Bamisaiye <code@olab.dev>
2021-09-19 14:54:29 -0400

    Create cc_nestedif.py
    Completed the Code Challenge: Nested If

---
+++ week1/challenges/cc_nestedif.py
@@ -0,0 +1,12 @@
+# Name: Ola B.
+# Email: code@olab.dev
+priceIsRight = 10
+
+if priceIsRight < 5:
+    print("Price is too low!")
+elif priceIsRight >= 5 and priceIsRight <= 9:
+    print("Price is almost there!")
+elif priceIsRight == 10:
+    print("Price is exactly that!")
+else:
+    print("Price is too high!")
