bf4fbcf30039764689a2df92799e2b69604c1e87
Ola Bamisaiye <code@olab.dev>
2021-10-02 13:39:38 -0400

    Update cc_reverse.py
    Reverse Exercise

--- week2/challenges/cc_reverse.py
+++ week2/challenges/cc_reverse.py
@@ -2,5 +2,11 @@
 # Email: code@olab.dev
 # Challenge 5 of Week 2
 
+# Cool info about slice():
+# https://stackoverflow.com/questions/509211/understanding-slice-notation
+def reverse(str):
+    return str[::-1]
+
+
 name = input("What is your name? ")
 print("Your name reversed is:", reverse(name))
