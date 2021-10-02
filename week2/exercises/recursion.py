5f675dac8b8da3aa47b5f8dc1050ebaf4c049306
Ola Bamisaiye <code@olab.dev>
2021-10-02 13:10:35 -0400

    Update recursion.py
    completion

--- week2/exercises/recursion.py
+++ week2/exercises/recursion.py
@@ -1,3 +1,26 @@
 # Name: Ola B.
 # Email: code@olab.dev
 # Exercise 7 of Week 2
+
+# Creating the fibonacci sequence; code that will get the number in the
+# Fibonacci seqeunce will be provided
+
+import sys
+
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+count = len(arguments)
+
+for x in sys.argv:
+    print("Argument: ", x)
+
+
+def rFib(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    return rFib(n-1) + rFib(n-2)
+
+
+print(rFib(int(sys.argv[1])))
