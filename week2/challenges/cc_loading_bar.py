5c9a5b99dd1af1e21503e27e0537304fde92673f
Ola Bamisaiye <code@olab.dev>
2021-10-02 08:32:22 -0400

    Update cc_loading_bar.py
    another step

--- week2/challenges/cc_loading_bar.py
+++ week2/challenges/cc_loading_bar.py
@@ -1,2 +1,10 @@
 for amount_loaded in range(100, 0, 1):
+    if amount_loaded == 25:
+        print("Something to do at 25")
+    elif amount_loaded == 50:
+        print("Something to do at 50")
+    elif amount_loaded == 75:
+        print("Something to do at 75")
+    elif amount_loaded == 100:
+        print("Something special at 100")
     print(amount_loaded)
