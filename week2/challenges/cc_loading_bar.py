1d15a25ea50af1bf0da1bf75821dfff00c8f77dd
Ola Bamisaiye <code@olab.dev>
2021-10-02 08:35:37 -0400

    Update cc_loading_bar.py
    Made an inclusive range setup

--- week2/challenges/cc_loading_bar.py
+++ week2/challenges/cc_loading_bar.py
@@ -1,10 +1,14 @@
-for amount_loaded in range(100, 0, 1):
+start = 0
+step = 5
+stop = 100 + step
+for amount_loaded in range(start, stop, step):
     if amount_loaded == 25:
-        print("Something to do at 25")
+        print("1/4 of the way there")
     elif amount_loaded == 50:
-        print("Something to do at 50")
+        print("1/2 of the way there")
     elif amount_loaded == 75:
-        print("Something to do at 75")
+        print("3/4 of the way there")
     elif amount_loaded == 100:
-        print("Something special at 100")
-    print(amount_loaded)
+        print("Loading complete!")
+    else:
+        print(amount_loaded)
