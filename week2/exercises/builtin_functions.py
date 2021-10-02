20f54ffec6e6a8a688f1260195900fa0a753e1f2
Ola Bamisaiye <code@olab.dev>
2021-10-02 09:00:49 -0400

    Create builtin_functions.py
    added comments and following the exercise

---
+++ week2/exercises/builtin_functions.py
@@ -0,0 +1,14 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Exercise 2 of Week 2
+# All builtin functions docs: https://docs.python.org/3/library/functions.html
+print("Math Functions")
+# About this⮯ built-in: https://careerkarma.com/blog/python-absolute-value/
+abs_int = abs(-1)
+print(abs_int)
+# About this⮯ built-in: https://www.geeksforgeeks.org/float-in-python/
+int_to_float = float(100)
+print(int_to_float)
+# About this⮯ built-in:
+float_to_int = int(1.23)
+print(float_to_int)
