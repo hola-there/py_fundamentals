0a5aa77664e1392aadc3f9c94396cab9ac7d9c18
Ola Bamisaiye <code@olab.dev>
2021-10-02 10:19:21 -0400

    Create convert_input.py
    Finished another exercise

---
+++ week2/exercises/convert_input.py
@@ -0,0 +1,8 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Exercise 3 of Week 2
+total_cash = 0
+user_input = input("Enter amount to transfer: ")
+total_cash = total_cash + float(user_input)
+print("Total cash is now: ")
+print(total_cash)
