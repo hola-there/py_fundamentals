8fdd1db53258eb71fad20d4a967b72f6c03f1226
Ola Bamisaiye <code@olab.dev>
2021-10-07 18:36:25 -0400

    Update app.py
    Task 2 Completed

--- week2/workshop/app.py
+++ week2/workshop/app.py
@@ -8,3 +8,10 @@ def atm_menu(name):
     print("------------------------------------------")
     print("| 3.    Withdraw    | 4.    Logout       |")
     print("------------------------------------------")
+
+
+print("          === Automated Teller Machine ===          ")
+name = input("Enter name to register: ")
+pin = input("Enter PIN: ")
+balance = 0
+print(name + " has been reigstered with a starting balence of $" + str(balance))
