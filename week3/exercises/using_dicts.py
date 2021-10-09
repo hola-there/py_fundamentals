ec17c31b066963f244140c926747f4362d8604c4
Ola Bamisaiye <code@olab.dev>
2021-10-09 12:47:50 -0400

    Create using_dicts.py
    starting point

---
+++ week3/exercises/using_dicts.py
@@ -0,0 +1,7 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Exercise 4 of Week 3
+
+state_capitals = {"Washington": "Olympia",
+                  "Oregon": "Salem", "California": "Sacramento"}
+print(len(state_capitals))
