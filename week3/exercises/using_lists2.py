27b1caf292715a6109f2858003456dd9c1e0c398
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:23:36 -0400

    Update using_lists2.py
    Updated the comment and modified list items whilst searching through the list

--- week3/exercises/using_lists2.py
+++ week3/exercises/using_lists2.py
@@ -1,37 +1,15 @@
 # Name: Ola B.
 # Email: code@olab.dev
-# Exercise 11 of Week 3
+# Exercise 2 of Week 3
 
 # Creating a list; note the use of commas & the []s
 states = ["Washington", "Oregon", "California"]
+
 # Iterating through a list
-for x in states:
-    print(x)
+for state in states:
+    print(state)
 
-# Retrieve values in a list
-''' 
-print(states[-1])
-print(states[-2])
-print(states[-3])
-'''
-
-# Modifying list values
-states[2] = "Arizona"
-# print(states)
-
-# Finding the length of a list
-# print(len(states))
-
-# Testing out List methods (functions that are tied to an object)
-
-# Adding an item to a list with the append list method
-states.append("New York")
-print(states)
-
-# Removing the last item to a list with the pop list method
-states.pop()
-print(states)
-
-# Removing a list item from a specific index
-states.pop(1)
-print(states)
+# Iterating through a list with modification of each index
+for state in states:
+    state = state.lower()
+    print(state)
