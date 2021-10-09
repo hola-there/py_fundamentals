b410344ae1063b1d07c8bed2d74798b669f6fffb
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:15:42 -0400

    Update using_lists.py
    Finding length of a list

--- week3/exercises/using_lists.py
+++ week3/exercises/using_lists.py
@@ -2,11 +2,19 @@
 # Email: code@olab.dev
 # Exercise 11 of Week 3
 
+# Creating a list; note the use of commas & the []s
 states = ["Washington", "Oregon", "California"]
-'''
+
+# Retrieve values in a list
+''' 
 print(states[-1])
 print(states[-2])
 print(states[-3])
 '''
+
+# Modifying list values
 states[2] = "Arizona"
-print(states)
+# print(states)
+
+# Finding the length of a list
+print(len(states))
