2dd37dd480c7ab844dbea5d8bdc5223583ae662a
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:33:16 -0400

    Create lists.py
    Starting point

---
+++ week3/challenges/lists.py
@@ -0,0 +1,12 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Challenge 1 of Week 3
+
+import random
+
+diamonds = ["AD", "2D", "3D", "4D", "5D",
+            "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+
+player_hand = []
+
+while diamonds:
