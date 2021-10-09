7903847ae57962de67b95f6d47af428b0453d113
Ola Bamisaiye <code@olab.dev>
2021-10-09 18:55:21 -0400

    Update using_sets.py
    access sets part 1

--- week3/exercises/using_sets.py
+++ week3/exercises/using_sets.py
@@ -12,7 +12,17 @@
 # Creating an empty set
 empty_set = set()
 
+# Trying out set values
 # numbers_set = {1, 2, 3, 4, 4} # duplicate values removed
 # numbers_set = {1, 2, 3, 4, [5, 6]}  # cannot use mutable data types in a set
 # numbers_set = {1, 2, 3, 4, (5, 6)}  # tuples are immutable so this is a okay
 # print(numbers_set)
+
+# accessing set values
+words_set = {"Alpha", "Bravo", "Charlie"}
+
+abcd = ""
+for word in words_set:
+    abcd += word
+# sets are unordered so the order of the items aren't set. different values per run
+print(abcd)
