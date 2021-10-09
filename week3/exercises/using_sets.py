48e4bc3d248e283822f94e31fe18619e7ca973cf
Ola Bamisaiye <code@olab.dev>
2021-10-09 18:48:45 -0400

    Create using_sets.py
    starting point

---
+++ week3/exercises/using_sets.py
@@ -0,0 +1,13 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Exercise 8 of Week 3
+
+# sets are an unordered collection of balues
+# sets are comma seperated
+# sets are not indexed..so no bracket notation
+# sets are mutable but cannot contain mutable data types
+# sets automagically removes duplicates
+# sets can be created empty with the set() function
+
+# Creating an empty set
+empty_set = set()
