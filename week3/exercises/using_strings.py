faa3596e27bd6d89e96f5f23846c93f1b80c95e8
Ola Bamisaiye <code@olab.dev>
2021-10-09 12:38:10 -0400

    Update using_strings.py
    Completed the exercise

--- week3/exercises/using_strings.py
+++ week3/exercises/using_strings.py
@@ -8,15 +8,22 @@ charlie"""
 print(my_string)
 print(multiline_string)'''
 
+# Printing a certain index of a string since it is a indexed sequence
 '''
 print(MY_STRING[0])
 print(MY_STRING[3])
 '''
 
+# Slicing Notation of a String
 print(MY_STRING[0:3])
 print(MY_STRING[:2])
 print(MY_STRING[2:])
 print(MY_STRING)
 
+# Iterate through a string
 for char in MY_STRING:
     print(char)
+
+# Test substring existence in a string
+print("pha" in MY_STRING)
+print("z" not in MY_STRING)
