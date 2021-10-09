a16da6b8d103c2374299f64b05132d82fc2c8300
Ola Bamisaiye <code@olab.dev>
2021-10-09 18:39:59 -0400

    Update using_tuples.py
    tuples object does not support item assignment due to being an immutable data type...so no modifications but the tuple variable can be updated though

--- week3/exercises/using_tuples.py
+++ week3/exercises/using_tuples.py
@@ -7,3 +7,6 @@ my_tuple = (1, 2, 3, 4, 5)
 # Accessing tuple values
 # print(my_tuple[0] + my_tuple[1]) # add first two numbers in the tuple
 print(my_tuple[-1] + my_tuple[-2])  # add last two numbers in the tuple
+
+# Modifying a tuple
+my_tuple[3] = 1
