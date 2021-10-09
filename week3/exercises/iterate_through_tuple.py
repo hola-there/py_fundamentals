77bc46a758c6302c2a714ccb4fa2d07cd3f090a0
Ola Bamisaiye <code@olab.dev>
2021-10-09 18:44:22 -0400

    Create iterate_through_tuple.py
    Completed exercise

---
+++ week3/exercises/iterate_through_tuple.py
@@ -0,0 +1,13 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Exercise 7 of Week 3
+
+# variables declared
+my_tuple = (1, 2, 3, 4, 5)
+total = 0
+
+# iterate through the tuple: my_tuple
+for n in my_tuple:
+    total += n
+
+print(total)
