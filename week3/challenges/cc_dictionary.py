3c2e9dd9ed925c59f20d2dc82b6d0bd5712b7107
Ola Bamisaiye <code@olab.dev>
2021-10-09 17:24:46 -0400

    Update cc_dictionary.py
    completed challenge!
    
    Still going to edit it to make it better from how I see fit though

--- week3/challenges/cc_dictionary.py
+++ week3/challenges/cc_dictionary.py
@@ -2,22 +2,26 @@
 # Email: code@olab.dev
 # Challenge 2 of Week 3
 
+# Declarations
 inches_snow = {"Monday": 2, "Tuesday": 4, "Wednesday": 5}
 
 
-def total_snowfall(dict_to_pass):
-    num = 0
-    for snow in dict_to_pass.values():
-        num += snow
-    print("Total snowfall inches:", num)
+def print_total_snowfall(inches_snow):
+    total_inches = 0
+    for inches in inches_snow.values():
+        total_inches += inches
+    print("Total snowfall inches:", total_inches)
 
 
 def new_snowfall_record(dict_to_pass):
-    day = input("Which day was the snowfall recorded?: ")
-    num_of_inches = input("How many inches fell?: ")
-    inches_snow[str(day)] = int(num_of_inches)
-    total_snowfall(dict_to_pass)
+    new_thursday_record = input("How many inches of snow fell on Thursday?: ")
+    if new_thursday_record.isnumeric():
+        dict_to_pass["Thursday"] = int(new_thursday_record)
+        #day = input("Which day was the snowfall recorded?: ")
+        #num_of_inches = input("How many inches fell?: ")
+        #inches_snow[str(day)] = int(num_of_inches)
+        print_total_snowfall(dict_to_pass)
 
 
-total_snowfall(inches_snow)
+print_total_snowfall(inches_snow)
 new_snowfall_record(inches_snow)
