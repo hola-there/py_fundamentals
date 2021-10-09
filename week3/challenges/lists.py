99d62ead79f4695e1048d717db6520502b39094e
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:47:48 -0400

    Update lists.py
    Completed Lists code challenge

--- week3/challenges/lists.py
+++ week3/challenges/lists.py
@@ -9,4 +9,26 @@ diamonds = ["AD", "2D", "3D", "4D", "5D",
 
 player_hand = []
 
+
+# Gets a hand from the subset of cards provided and adds it to the player's hand
+def get_new_hand():
+    random_card = random.randint(0, (len(diamonds) - 1))
+    player_hand.append(diamonds[random_card])
+    diamonds.pop(random_card)
+
+
+def print_deck(deck_name, deck):
+    print("The contents of '" + str(deck_name) + "' is: " + str(deck))
+
+
 while diamonds:
+    user_choice = input(
+        "Press enter to pick a card; or 'Q' then enter to quit: ")
+    if user_choice.upper() == "Q":
+        print("Exiting now...!")
+        break
+    get_new_hand()
+    print_deck("Diamonds deck", diamonds)
+    print_deck("Player deck", player_hand)
+    if not diamonds:
+        print("There are no more cards to pick.")
