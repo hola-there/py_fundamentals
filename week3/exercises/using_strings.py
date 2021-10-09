12dcee92f9748c71776c71c72ada4053b4e452fe
Ola Bamisaiye <code@olab.dev>
2021-10-09 12:30:12 -0400

    Update using_strings.py
    access and slice characters

--- week3/exercises/using_strings.py
+++ week3/exercises/using_strings.py
@@ -8,5 +8,12 @@ charlie"""
 print(my_string)
 print(multiline_string)'''
 
+'''
 print(MY_STRING[0])
 print(MY_STRING[3])
+'''
+
+print(MY_STRING[0:3])
+print(MY_STRING[:2])
+print(MY_STRING[2:])
+print(MY_STRING)
