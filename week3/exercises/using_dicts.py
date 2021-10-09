d941694d86dde56e5de78efab4782d7c13cb9676
Ola Bamisaiye <code@olab.dev>
2021-10-09 12:49:51 -0400

    Update using_dicts.py
    accessed dictionary value

--- week3/exercises/using_dicts.py
+++ week3/exercises/using_dicts.py
@@ -2,6 +2,10 @@
 # Email: code@olab.dev
 # Exercise 4 of Week 3
 
+# Creation of a dictionary
 state_capitals = {"Washington": "Olympia",
                   "Oregon": "Salem", "California": "Sacramento"}
-print(len(state_capitals))
+# print(len(state_capitals)) # Length of the dictionary according to the # of key:value pairs
+
+# Accessing dictionary values
+print(state_capitals["Washington"])
