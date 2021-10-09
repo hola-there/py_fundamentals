419b6ad75fa2f9f7c9736442184f4aa2c198995d
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:13:38 -0400

    Update using_lists.py
    Modified list value with index

--- week3/exercises/using_lists.py
+++ week3/exercises/using_lists.py
@@ -3,7 +3,10 @@
 # Exercise 11 of Week 3
 
 states = ["Washington", "Oregon", "California"]
-
+'''
 print(states[-1])
 print(states[-2])
 print(states[-3])
+'''
+states[2] = "Arizona"
+print(states)
