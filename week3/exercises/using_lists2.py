2fff25275288cd63236e138aa763bc9f11b826b6
Ola Bamisaiye <code@olab.dev>
2021-10-09 10:30:39 -0400

    Update using_lists2.py
    Played with slicing notation

--- week3/exercises/using_lists2.py
+++ week3/exercises/using_lists2.py
@@ -23,3 +23,9 @@ print("Washington" not in states)
 states2 = ["Arizona", "Ohio", "Lousisana", "Maryland"]
 best_states = states + states2
 print(best_states)
+
+# Slicing certain values from a list (Slicing Notation)
+print(best_states[1:3])  # only collect from a certain index to a certain index
+print(best_states[:2])  # Only collect list items before a certain index
+print(best_states[::-1])  # Reverse the list
+print(best_states[4:])  # Only collect list items after a certain index
