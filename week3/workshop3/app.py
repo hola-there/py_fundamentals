ba42e384d88b49e7a8e006e48aafe013ddcb0923
Ola Bamisaiye <code@olab.dev>
2021-10-09 19:52:34 -0400

    Update app.py
    Task #3 for Workshop #3 is completed

--- week3/workshop3/app.py
+++ week3/workshop3/app.py
@@ -9,8 +9,33 @@ from donations_pkg.homepage import show_homepage
 database = {"admin", "password123"}
 donations = []
 authorized_user = ""
-show_homepage()
-if authorized_user == "":
-    print("You must be logged in to donate.")
-else:
-    print("Logged in as:", authorized_user)
+run_donations = True
+
+
+def login_check():
+    if authorized_user == "":
+        print("You must be logged in to donate.")
+    else:
+        print("Logged in as:", authorized_user)
+
+
+def homepage_option_selection():
+    login_check()
+    choice = input("Choose and Option: ")
+    if (choice.isnumeric() and int(choice) == 1) or choice.lower() == "login":
+        print("TODO: Write Login Functionality")
+    elif (choice.isnumeric() and int(choice) == 2) or choice.lower() == "register":
+        print("TODO: Write Register Functionality")
+    elif (choice.isnumeric() and int(choice) == 3) or choice.lower() == "donate":
+        print("TODO: Write Donate Functionality")
+    elif (choice.isnumeric() and int(choice) == 4) or choice.lower() == "show donations":
+        print("TODO: Write Show Donations Functionality")
+    elif (choice.isnumeric() and int(choice) == 5) or choice.lower() == "exit":
+        print("Goodbye...exiting now")
+        global run_donations
+        run_donations = False
+
+
+while run_donations:
+    show_homepage()
+    homepage_option_selection()
