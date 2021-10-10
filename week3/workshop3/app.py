796d1c35b8931e0595681bebca3735129b4ecbb1
Ola Bamisaiye <code@olab.dev>
2021-10-09 20:55:49 -0400

    Completed Task #5

--- week3/workshop3/app.py
+++ week3/workshop3/app.py
@@ -3,8 +3,7 @@
 # Main program for Workshop #3 from Week 3
 
 from donations_pkg.homepage import show_homepage
-from donations_pkg.user import login
-
+from donations_pkg.user import login, register
 
 # Declaring variables
 database = {"admin": "password123", }
@@ -21,17 +20,26 @@ def login_check():
         print("Logged in as:", authorized_user, "\n")
 
 
+# Checks if the result of whether a user is registered already or not
+def can_register(database_passed, username_passed, password_passed):
+    if authorized_user != "":
+        database_passed[str(username_passed)] = password_passed
+
+
 # user is presented with options
 def homepage_option_selection():
     login_check()
+    global authorized_user
     choice = input("Choose and Option: ")
     if (choice.isnumeric() and int(choice) == 1) or choice.lower() == "login":
         username = input("\nPlease enter your account's username: ")
         password = input("Please enter your account's password: ")
-        global authorized_user
         authorized_user = login(database, username, password)
     elif (choice.isnumeric() and int(choice) == 2) or choice.lower() == "register":
-        print("TODO: Write Register Functionality")
+        username = input("\nPlease enter your account's username: ")
+        password = input("Please enter your account's password: ")
+        authorized_user = register(database, username)
+        can_register(database, username, password)
     elif (choice.isnumeric() and int(choice) == 3) or choice.lower() == "donate":
         print("TODO: Write Donate Functionality")
     elif (choice.isnumeric() and int(choice) == 4) or choice.lower() == "show donations":
--- week3/workshop3/donations_pkg/user.py
+++ week3/workshop3/donations_pkg/user.py
@@ -3,7 +3,7 @@
 # Part of Workshop #3 from Week 3
 
 
-#
+# Ensures that the correct datatype is passed
 def login(database_passed, username_passed, password_passed):
     # isinstance source: https://docs.python.org/3/library/functions.html#isinstance
     if isinstance(database_passed, dict) and isinstance(username_passed, str) and isinstance(password_passed, str):
@@ -17,5 +17,22 @@ def login(database_passed, username_passed, password_passed):
                 return ""
             else:
                 print(
-                    "Password & Username does not exist. If you'd like you should register an account")
+                    "Password & Username does not exist. Please register an account")
                 return ""
+    else:
+        print("Incorrect data type passed; please try again")
+        return ""
+
+
+def register(database_passed, username_passed):
+    # isinstance source: https://docs.python.org/3/library/functions.html#isinstance
+    if isinstance(database_passed, dict) and isinstance(username_passed, str):
+        if username_passed in database_passed.keys():
+            print("Username already registered.")
+            return ""
+        else:
+            print("Successfully Registered:", username_passed)
+            return username_passed
+    else:
+        print("Incorrect data type passed; please try again")
+        return ""
