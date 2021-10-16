8e57271acd14da46fda8b5e9e3325d5a33b6ca3a
Ola Bamisaiye <code@olab.dev>
2021-10-16 11:17:29 -0400

    Update using_class1.py
    instance attributes discussed and played with
    
    changes to the instance attribute belongs to the instance and the instance alone

--- week4/exercises/using_class1.py
+++ week4/exercises/using_class1.py
@@ -2,11 +2,11 @@
 # Email: code@olab.dev
 # Challenge 1 of Week 4
 
+'''
 # Capitalize the first letter of an object name in python
 # The blueprint of a object to come
 class Player:
-    max_hp = 4000
-
+    max_hp = 4000  # this is a class attribute
 
 # Instantiating the object
 # actually creating something from a class: following through on the blueprint
@@ -15,3 +15,30 @@ player1 = Player()
 print(player1.max_hp)
 player2 = Player()
 print(player2.max_hp)
+
+# modifying the class attribute which also changes the objects created of the object
+Player.max_hp = 5000
+print(player1.max_hp)
+print(player2.max_hp)
+'''
+
+# start of using the instance attribute
+
+
+class Player:
+    # constructor method must have a parameter list; self is required
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp  # dynamic class instance attribute
+        self.score = 0
+
+
+# Python makes sure that the reference to the object is passed
+# which is why self doesn't need to be specified
+player1 = Player("Aaron", 1200)
+player2 = Player("Irene", 1300)
+
+player1.hp += 500
+player1.score += 10
+print("P1:", player1.name, " -- HP: ", player1.hp, " -- SCORE: ", player1.score)
+print("P2:", player2.name, " -- HP: ", player2.hp, " -- SCORE: ", player2.score)
