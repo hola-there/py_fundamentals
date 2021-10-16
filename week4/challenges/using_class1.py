0b1824d52fff670e9f659b686ef2e7cfe0959aa0
Ola Bamisaiye <code@olab.dev>
2021-10-16 10:58:32 -0400

    Moved and Changed using_class1.py
    Part 1 of using classes

--- week4/challenges/using_class1.py
+++
@@ -1,3 +0,0 @@
-# Name: Ola B.
-# Email: code@olab.dev
-# Challenge 1 of Week 4
---
+++ week4/exercises/using_class1.py
@@ -0,0 +1,17 @@
+# Name: Ola B.
+# Email: code@olab.dev
+# Challenge 1 of Week 4
+
+# Capitalize the first letter of an object name in python
+# The blueprint of a object to come
+class Player:
+    max_hp = 4000
+
+
+# Instantiating the object
+# actually creating something from a class: following through on the blueprint
+player1 = Player()
+# Access a class's attribute with dot notation
+print(player1.max_hp)
+player2 = Player()
+print(player2.max_hp)
